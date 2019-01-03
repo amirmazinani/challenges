de09b95e607cb9294fc8d71c16f74f2235773c3c
amirmazinani <amir_mz75@hotmail.com>
2019-01-04 00:32:21 +0330

    start challenge 1

---
+++ challenges/1/index.php
@@ -0,0 +1,32 @@
+<section class="-b-c-pink-1 -h-100vh -py-20 -bx-s-b">
+    <main>
+        <div class="-dis-f -jus-c-c">
+
+            <div class="item -pos-r">
+                <div>
+                    <p class="title -fnt-lalezar -pos-a -left-50">سایت شخصی</p>
+                    <div class="image -transition">
+                        <img class="-brd-r-10 -bxsh-3" src="/uploads/images/img.jpg" alt="image" height="250px">
+                    </div>
+                    <div class="content -transition -h-100p -p-20 -fnt-s-smaller -c-gray-5 -bx-s-b -b-c-white -bxsh-3 -brd-r-10 -dis-f -f-w-w -al-it-e">
+                        <div class="-w-11">
+                            <p class="-opa-0 -transition -pb-10 -bx-s-b">
+                                <span class="-c-gray-6 -pl-10">عنوان: </span>
+                                <span>طراحی سایت شخصی</span>
+                            </p>
+                            <p class="-opa-0 -transition -pb-10 -bx-s-b">
+                                <span class="-c-gray-6 -pl-10">طراح: </span>
+                                <span>امیرحسین مزینانی</span>
+                            </p>
+                            <p class="-opa-0 -transition -pb-10 -bx-s-b">
+                                <span class="-c-gray-6 -pl-10">تاریخ: </span>
+                                <span>23 شهریور</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </main>
+</section>
