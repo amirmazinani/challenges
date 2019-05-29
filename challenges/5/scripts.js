29946d6320bcaf0e42250a8f099657e1fe653539
amirmazinani <amir_mz75@hotmail.com>
2019-05-29 12:24:37 +0430

    continue challenge 5 : complete

--- challenges/5/scripts.js
+++ challenges/5/scripts.js
@@ -10,35 +10,27 @@ app.directive('autoTooltip', ['$interval', '$timeout', function ($interval, $tim
                 let button = element[0];
                 let container = document.querySelector(scope.container);
                 let dropdown = element[0].querySelector(scope.dropdown);
-                let container_points = {
-                    'top': container.offsetTop,
-                    'right': container.offsetLeft + container.clientWidth,
-                    'bottom': container.offsetTop + container.clientHeight,
-                    'left': container.offsetLeft,
-                };
 
                 function setButtonDistance() {
-                    angular.element(dropdown).css({'display':'block','visibility':'hidden','pointer-event':'none'});
+                    angular.element(dropdown).css({
+                        'visibility': 'hidden',
+                        'pointer-event': 'none',
+                        'display': 'block'
+                    });
                     let button_distances = {
                         'top': button.offsetTop,
                         'right': container.clientWidth - (button.offsetLeft + button.clientWidth),
                         'bottom': container.clientHeight - (button.offsetTop + button.clientHeight),
                         'left': button.offsetLeft,
                     };
-                    button_distances = sortObj(button_distances);
-                    $timeout(function () {angular.element(dropdown).css({'display':'none','visibility':'visible','pointer-event':'auto'});});
-                    return button_distances;
-                }
-
-                function sortObj(o) {
-                    let sortable = [];
-                    for (let i in o) sortable.push([i, o[i]]);
-                    sortable.sort(function (a, b) {
-                        return b[1] - a[1];
+                    $timeout(function () {
+                        angular.element(dropdown).css({
+                            'display': 'none',
+                            'visibility': 'visible',
+                            'pointer-event': 'auto'
+                        });
                     });
-                    let obj_sort = {};
-                    for (let i in sortable) obj_sort[sortable[i][0]] = sortable[i][1];
-                    return obj_sort
+                    return button_distances;
                 }
 
                 function setState(dropdown, distance) {
@@ -51,10 +43,10 @@ app.directive('autoTooltip', ['$interval', '$timeout', function ($interval, $tim
                         state += '3';
                     } else {
                         alert('not space in left and right');
-                        console.log('distance left :'+distance['left']);
-                        console.log('distance right :'+distance['right']);
-                        console.log('dropdown width :'+dropdown.clientWidth);
-                        console.log('dropdown height :'+dropdown.clientHeight);
+                        console.log('distance left :' + distance['left']);
+                        console.log('distance right :' + distance['right']);
+                        console.log('dropdown width :' + dropdown.clientWidth);
+                        console.log('dropdown height :' + dropdown.clientHeight);
                     }
                     if (distance['top'] >= dropdown.clientHeight && distance['bottom'] >= dropdown.clientHeight) {
                         state += '1';
@@ -64,10 +56,10 @@ app.directive('autoTooltip', ['$interval', '$timeout', function ($interval, $tim
                         state += '3';
                     } else {
                         alert('not space in top and bottom');
-                        console.log('distance top :'+distance['top']);
-                        console.log('distance bottom :'+distance['bottom']);
-                        console.log('dropdown width :'+dropdown.clientWidth);
-                        console.log('dropdown height :'+dropdown.clientHeight);
+                        console.log('distance top :' + distance['top']);
+                        console.log('distance bottom :' + distance['bottom']);
+                        console.log('dropdown width :' + dropdown.clientWidth);
+                        console.log('dropdown height :' + dropdown.clientHeight);
                     }
                     return Number(state)
                 }
@@ -125,21 +117,15 @@ app.directive('autoTooltip', ['$interval', '$timeout', function ($interval, $tim
                     }
                 }
 
-
-                angular.element(button).on('click',function () {
-                    let button_distances = setButtonDistance();
-                    let state = setState(dropdown, button_distances);
-                    setStyle(dropdown, state);
-                    console.log(button_distances);
-                    console.log(state);
-
-                    $timeout(function () {
-                        if(angular.element(dropdown).css('display')==='block'){
-                            angular.element(dropdown).css({'display':'none'});
-                        }else if(angular.element(dropdown).css('display')==='none'){
-                            angular.element(dropdown).css({'display':'block'});
-                        }
-                    },10)
+                angular.element(button).on('click', function () {
+                    if (angular.element(dropdown).css('display') === 'block') {
+                        angular.element(dropdown).css({'display': 'none'});
+                    } else if (angular.element(dropdown).css('display') === 'none') {
+                        let button_distances = setButtonDistance();
+                        let state = setState(dropdown, button_distances);
+                        setStyle(dropdown, state);
+                        $timeout(function () {angular.element(dropdown).css({'display': 'block'})},10);
+                    }
                 })
 
             }, 1000)
